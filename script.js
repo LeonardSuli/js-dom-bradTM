cb8a4e8f7f49faf8a47aac29775623bb25367b5b
LeonardSuli <leonardsuli1991@gmail.com>
2025-10-11 18:28:54 +0200

    add eventlistener that removed the li if click X button

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const form = document.getElementById('addForm');
 const itemList = document.getElementById('items');
 const text = document.getElementById('item');
+const filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
 
+// Delete event 
+itemList.addEventListener('click', removeItem)
+
 // Add Item
 function addItem(e) {
+
     e.preventDefault();
-    // console.log(text.value);
 
-    const itemMarkup = `<li class="list-group-item d-flex justify-content-between">${text.value}<button
-                        class="btn btn-danger btn-sm delete">X</button>
-                </li>`
+    const newItemMarkup = `<li class="list-group-item d-flex justify-content-between">
+                            ${text.value}
+                            <button class="btn btn-danger btn-sm delete">X</button>
+                        </li>`
 
-    itemList.insertAdjacentHTML('beforeend', itemMarkup)
+    itemList.insertAdjacentHTML('beforeend', newItemMarkup)
 
     text.value = '';
+
+}
+
+// Remove item
+function removeItem(e) {
+
+    // e.preventDefault();
+
+    console.log('hellooo');
+
+    if (e.target.classList.contains('delete')) {
+        e.target.parentElement.remove();
+    }
+
 
 }
